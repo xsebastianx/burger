0d2ca4f8988a65fd8fd63a1dc2a9346528c563e2
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2020-02-15 10:42:38 -0500

    Created app

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, eaten) VALUES ('beyond burger', false);
+INSERT INTO burgers (burger_name, eaten) VALUES ('cheese burger', false);
+INSERT INTO burgers (burger_name, eaten) VALUES ('turkey burger', false);
